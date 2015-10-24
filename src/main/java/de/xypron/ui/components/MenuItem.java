1e4d9eec98ed85b9a0a86fc26780c2d709ca331a
Heinrich Schuchardt <xypron.glpk@gmx.de>
2015-10-24 17:55:55 +0000

    MenuItem support icons

--- src/main/java/de/xypron/ui/components/MenuItem.java
+++ src/main/java/de/xypron/ui/components/MenuItem.java
@@ -15,7 +15,9 @@
  */
 package de.xypron.ui.components;
 
+import de.xypron.util.IconBuffer;
 import de.xypron.util.IdeText;
+import de.xypron.util.MenuItemIcon;
 import de.xypron.util.MenuItemText;
 import java.awt.event.ActionEvent;
 import java.lang.reflect.Method;
@@ -23,20 +25,22 @@ import javax.swing.JMenuItem;
 
 /**
  * Menu item.
+ *
  * @author Heinrich Schuchardt
  */
 public class MenuItem extends JMenuItem {
 
     /**
      * Creates a menu item.
+     *
      * @param obj object to be invoked by menu item
      * @param methodName method to be invoked by menu item
      */
     public MenuItem(Object obj, String methodName) {
-        MenuItemText annotation;
         Class clas;
         Method method = null;
         String text;
+        String iconName;
 
         clas = obj.getClass();
 
@@ -54,13 +58,23 @@ public class MenuItem extends JMenuItem {
             break;
         }
         text = methodName;
+        iconName = null;
         if (method
                 != null) {
-            annotation = method.getAnnotation(MenuItemText.class);
-            if (annotation != null) {
-                text = annotation.value();
+            MenuItemText textAnnotation;
+            MenuItemIcon iconAnnotation;
+            textAnnotation = method.getAnnotation(MenuItemText.class);
+            if (textAnnotation != null) {
+                text = textAnnotation.value();
                 if (text.length() == 0) {
                     text = methodName;
+                }
+            }
+            iconAnnotation = method.getAnnotation(MenuItemIcon.class);
+            if (iconAnnotation != null) {
+                iconName = iconAnnotation.value();
+                if (text.length() == 0) {
+                    iconName = null;
                 }
             }
         }
@@ -70,6 +84,10 @@ public class MenuItem extends JMenuItem {
         }
 
         this.addActionListener(new MenuAction(obj, method));
+
+        if (iconName != null) {
+            this.setIcon(IconBuffer.getIcon(iconName));
+        }
     }
-    
+
 }
